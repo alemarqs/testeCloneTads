5ce862eb62b2ff32975aae7c98485e116e3a0339
brunloyola02 <larryisreal1d>
2020-02-14 10:08:55 -0300

    par ou impar

--- teste.c
+++ teste.c
@@ -3,6 +3,18 @@
 
 int main(){
 
+    int num;
+
+    printf("\n\tDigite um número: ");
+    scanf("%i", &num);
+
+    if(num % 2 == 0){
+        printf("\n\tO número %i é PAR ", num);
+    }else{
+        printf("\n\tO número %i é IMPAR ", num);
+    }
+    
+    printf("\n");
 
     return 0;
 }
